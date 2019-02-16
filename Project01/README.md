9c1a14bac1004fd35d58d10e21253a85c95ece77
Zoe0709 <ningh4@mcmaster.ca>
2019-02-16 05:06:49 +0000

    part 1 submission

---
+++ Project01/README.md
@@ -0,0 +1,16 @@
+Porject01
+
+Script Input
+When the script project_analyze.sh is executed, a select menu will appear. You can input the name of options to 
+make corresponding option executed.
+
+Option 1) TODO
+	This part of the script can put each line of every file in my repository with the tag #TODO into a file 
+	called todo.log. To execute this feature, please input TODO when the select menu appears.
+
+Option 2) FileTypeCount 
+	This part of the script can output the number of HTML, Javascript, CSS, Python, Haskell and bashscript file 
+	in my repository. To execute this feature, please input FileTypeCount when the select menu appears.
+
+Option 3) DeletetmpFiles
+	This part of the script can delete all untracked files ending in the extension .tmp.
---
+++ Project01/project_analyze.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd ..
+echo "Please choose your option: 1)TODO 2)FileTypeCount 3)DeletetmpFiles"
+read var
+case $var in
+	TODO)
+
+	    (grep -r "#TODO" --exclude="todo.log" --exclude="project_analyze.sh" .)> todo.log
+	    echo "Command TODO Executed."
+	    ;;
+	FileTypeCount)
+	    var1=$(find . -iname "*.html" | wc -l)
+	    var2=$(find . -iname "*.js" | wc -l)
+	    var3=$(find . -iname "*.css" | wc -l)
+	    var4=$(find . -iname "*.py" | wc -l)
+	    var5=$(find . -iname "*.hs" | wc -l)
+	    var6=$(find . -iname "*.sh" | wc -l) 
+	    echo "HTML: $var1, Javascript: $var2, CSS: $var3, Python: $var4, Haskell: $var5, Bash Script: $var6"
+	    ;;
+	DeletetmpFiles)
+	    tfile=$(git ls-files *.tmp . --exclude-standard --others)
+	    rm tfile
+	    echo "Command DtmpF Executed."
+	    ;;
+esac
